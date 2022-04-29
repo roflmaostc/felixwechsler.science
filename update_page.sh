611eda2126553429c101e1862aa7ba18c5404f05
roflmaostc <fxw+git@mailbox.org>
2022-04-29 10:36:42 +0200

    Fix script

--- update_page.sh
+++ update_page.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # git pull
-julia -e "using Pkg; Pkg.activate(\"/home/fxw/Documents/personal_webpage/felixwechsler.science/.\"); using Franklin; Franklin.serve()"
+julia -e "using Pkg; Pkg.activate(\".\"); using Franklin; Franklin.serve()"
